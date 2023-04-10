49c7db0cc4fd19ed21ecbe214fd59c6160ad2cec
raneemr05 <57775695+raneemr05@users.noreply.github.com>
2023-04-10 11:29:00 +0100

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero/Hero";
 import './App.css'
 import Companies from "./components/Companies/Companies";
 import Properties from "./components/Properties/Properties";
-import Value from "./components/value/value";
+import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
 import CTA from "./components/CTA/CTA";
 import Footer from "./components/Footer/Footer";
